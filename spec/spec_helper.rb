856c9dfef05158dd96334ddc2af2964497dfd578
holyshared <holy.shared.design@gmail.com>
2017-02-04 20:10:27 +0900

    add coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,27 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.add_filter do |source_file|
+  filename = source_file.filename
+  filename =~ /spec|fixtures|helpers/
+end
+
+SimpleCov.start
+
 require 'yaml'
 require 'yaml-translator'
 require 'helpers/locale_helper'
+
+if ENV['COVERALLS_REPO_TOKEN']
+  require 'coveralls'
+  Coveralls.wear!
+else
+  SimpleCov.formatters = [
+    SimpleCov::Formatter::Console
+  ]
+end
 
 RSpec.configure do |c|
   c.include Helpers::LocaleHelper
