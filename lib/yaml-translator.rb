a280f8528f94356289a8b8056f01c850ff6a1ffc
holyshared <holy.shared.design@gmail.com>
2017-02-08 19:00:25 +0900

    remove old api

--- lib/yaml-translator.rb
+++ lib/yaml-translator.rb
@@ -8,14 +8,6 @@ require 'yaml-translator/adapters'
 
 module YamlTranslator
   class << self
-    def load(s)
-      Locale.load(s)
-    end
-
-    def load_file(file)
-      Locale.load_file(file)
-    end
-
     def create(name, options={})
       adapter = Adapters.find_and_create(name, options)
       Translator.new(adapter)
