bdd77919d5ade5063e53266efe5205526e2e30e9
holyshared <holy.shared.design@gmail.com>
2017-02-08 20:12:27 +0900

    fix spec

--- spec/yaml-translator/translator_spec.rb
+++ spec/yaml-translator/translator_spec.rb
@@ -1,36 +1,21 @@
 describe YamlTranslator::Translator do
   let(:translator) { YamlTranslator::Translator.new }
-  describe '#translate' do
-    context 'when have root' do
-      let(:yaml_tree) { load_yaml(:with_root) }
-      let(:locale) { YamlTranslator::Locale.new(yaml_tree, :en) }
-      it 'translated tree' do
-        translated_locale = translator.translate(locale)
-        expect(translated_locale.values).to eq(locale.values)
-      end
-    end
-    context 'when have root' do
-      let(:yaml_tree) { load_yaml(:no_root) }
-      let(:locale) { YamlTranslator::Locale.new(yaml_tree, :en) }
-      it 'translated tree' do
-        translated_locale = translator.translate(locale)
-        expect(translated_locale.values).to eq(locale.values)
-      end
-    end
-  end
   describe '#file' do
-    let(:path) { yaml_path(:en) }
+    let(:path) { yaml_path(:ja) }
     let(:expected_locale) { YamlTranslator::Locale.load_file(path) }
     it 'translate files' do
-      result = translator.file(path).to(:en)
+      result = translator.file(path).to(:ja)
+      expect(result.lang).to eq(:ja)
       expect(result.values).to eq(expected_locale.values)
     end
   end
   describe '#string' do
+    let(:path) { yaml_path(:ja) }
     let(:yaml_source) { yaml_contents(:en) }
-    let(:expected_locale) { YamlTranslator::Locale.load(yaml_source) }
+    let(:expected_locale) { YamlTranslator::Locale.load_file(path) }
     it 'translate yaml string' do
-      result = translator.string(yaml_source).to(:en)
+      result = translator.string(yaml_source).to(:ja)
+      expect(result.lang).to eq(:ja)
       expect(result.values).to eq(expected_locale.values)
     end
   end
