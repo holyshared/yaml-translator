1886de8d0e2b490bdb86be4817462a5456247955
holyshared <holy.shared.design@gmail.com>
2017-02-10 15:54:56 +0900

    add zh-CN case

--- spec/yaml-translator/translator_spec.rb
+++ spec/yaml-translator/translator_spec.rb
@@ -10,13 +10,24 @@ describe YamlTranslator::Translator do
     end
   end
   describe '#string' do
-    let(:path) { yaml_path(:ja) }
     let(:yaml_source) { yaml_contents(:en) }
-    let(:expected_locale) { YamlTranslator::Locale.load_file(path) }
-    it 'translate yaml string' do
-      result = translator.string(yaml_source).to(:ja)
-      expect(result.lang).to eq(:ja)
-      expect(result.values).to eq(expected_locale.values)
+    context 'when ja' do
+      let(:path) { yaml_path(:ja) }
+      let(:expected_locale) { YamlTranslator::Locale.load_file(path) }
+      it 'translate yaml string' do
+        result = translator.string(yaml_source).to(:ja)
+        expect(result.lang).to eq(:ja)
+        expect(result.values).to eq(expected_locale.values)
+      end
+    end
+    context 'when zh-CN' do
+      let(:path) { yaml_path(:'zh-CN') }
+      let(:expected_locale) { YamlTranslator::Locale.load_file(path) }
+      it 'translate yaml string' do
+        result = translator.string(yaml_source).to(:'zh-CN')
+        expect(result.lang).to eq(:'zh-CN')
+        expect(result.values).to eq(expected_locale.values)
+      end
     end
   end
 end
