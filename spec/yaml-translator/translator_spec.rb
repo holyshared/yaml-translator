7d0bf1f728853145e198687bad5e53cb7c281b0d
holyshared <holy.shared.design@gmail.com>
2017-02-11 14:56:03 +0900

    add translate case

--- spec/yaml-translator/translator_spec.rb
+++ spec/yaml-translator/translator_spec.rb
@@ -1,12 +1,23 @@
 describe YamlTranslator::Translator do
   let(:translator) { YamlTranslator::Translator.new }
   describe '#file' do
-    let(:path) { yaml_path(:ja) }
-    let(:expected_locale) { YamlTranslator::Locale.load_file(path) }
-    it 'translate files' do
-      result = translator.file(path).to(:ja)
-      expect(result.lang).to eq(:ja)
-      expect(result.values).to eq(expected_locale.values)
+    context 'when ja' do
+      let(:path) { yaml_path(:ja) }
+      let(:expected_locale) { YamlTranslator::Locale.load_file(path) }
+      it 'translate files' do
+        result = translator.file(path).to(:ja)
+        expect(result.lang).to eq(:ja)
+        expect(result.values).to eq(expected_locale.values)
+      end
+    end
+    context 'when zh-CN' do
+      let(:path) { yaml_path(:'zh-CN') }
+      let(:expected_locale) { YamlTranslator::Locale.load_file(path) }
+      it 'translate files' do
+        result = translator.file(path).to(:'zh-CN')
+        expect(result.lang).to eq(:'zh-CN')
+        expect(result.values).to eq(expected_locale.values)
+      end
     end
   end
   describe '#string' do
