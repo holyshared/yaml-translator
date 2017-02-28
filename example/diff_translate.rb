133e6138910797404be47b754031a93d82a43bef
holyshared <holy.shared.design@gmail.com>
2017-02-28 10:40:10 +0900

    add diff nothing case

--- example/diff_translate.rb
+++ example/diff_translate.rb
@@ -3,11 +3,16 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'yaml'
 require 'yaml-translator'
 
-dir = File.join(File.dirname(__FILE__), 'diff/some')
 translator = ::YamlTranslator.create(
   :google_translate,
   api_key: ENV['GOOGLE_TRANSLATE_API_KEY']
 )
 
-diff_locale = translator.file("#{dir}/en.yml").diff("#{dir}/new.en.yml")
-diff_locale.to(:ja).save_to("/tmp")
+diff_some_dir = File.join(File.dirname(__FILE__), 'diff/some')
+diff_locale = translator.file("#{diff_some_dir}/en.yml").diff("#{diff_some_dir}/new.en.yml")
+diff_locale.to(:ja).save_to("/tmp", prefix: 'some_diff.')
+
+# diff nothing!!
+diff_none_dir = File.join(File.dirname(__FILE__), 'diff/none')
+diff_locale = translator.file("#{diff_none_dir}/en.yml").diff("#{diff_none_dir}/new.en.yml")
+diff_locale.to(:ja).save_to("/tmp", prefix: 'no_diff.')
